6ba4f500cf0ab8d4fd34e4686c2c13e873c41dca
zhangdoa <zhangandhang@gmail.com>
2017-09-11 17:58:54 +0800

    optimize spider.

--- PMovieFestival/PMovieFestival.py
+++ PMovieFestival/PMovieFestival.py
@@ -1,48 +1,38 @@
-# coding:utf-8
 __author__ = 'zhangdoa'
+
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import time
 import re
 
 class MovieFestival:
 
     def __init__(self):
-        self.getAll = False
         self.FestivalTitle = '柏林国际电影节'
         self.AwardTitle = ['最佳影片', '金熊奖']
         self.DoubanURL = 'https://movie.douban.com/awards/berlinale/'
-        self.FirstYear = 1951
-        self.LastYear = 2017
-        self.StartYear = self.FirstYear
-        self.EndYear = self.LastYear
-        self.LeakYear = []
-        self.LeakYearOffset = 0
-        self.MovieInfo = []
 
-    def getSingleUrl(self, year):
-        self.LeakYearOffset = 0
-        if year in self.LeakYear:
-            print(str(year) + '年未举办' + self.FestivalTitle + '\n')
-            return
-        for i in self.LeakYear:
-            if year > i:
-                self.LeakYearOffset+=1
-        URL = self.DoubanURL + str(year - self.FirstYear - self.LeakYearOffset + 1) + '/'
+    def writeToFile(self, fileLocation, data):
+        with (open(fileLocation + self.FestivalTitle + '.txt', 'a', encoding="utf-8-sig")) as m:
+            m.write(data)
+            m.write(u"\r\n")
+
+    def getSingleYearUrl(self, year, url):
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'}
-        res = requests.get(URL)
+        res = requests.get(url)
         soup = BeautifulSoup(res.text, 'html.parser')
-        print('正在抓取' + self.FestivalTitle + '(' + str(year - self.FirstYear - self.LeakYearOffset + 1) + ')' + str(year) + self.AwardTitle[0] + 'URL...')
+        print('正在抓取' + self.FestivalTitle + str(year) + self.AwardTitle[0])
         for mod in soup.find_all(class_ = 'mod'):
             for dt in mod.find_all('dt'):
                 for string in dt.stripped_strings:
                     if string in self.AwardTitle:
                         for link in (dt.next_sibling.next_sibling).find_all('a'):
-                            self.MovieInfo.append(self.FestivalTitle + "||" + str(year - self.FirstYear - self.LeakYearOffset + 1) + '||' + self.AwardTitle[0] + '||' + link.string + '||' + link.get('href') + '\n')                          
-                            print(link.get('href'))
+                            self.writeToFile('D:/', self.FestivalTitle + "||" + str(year) + '||' + self.AwardTitle[0] + '||' + link.string + '||' + link.get('href') + '||')
         print('抓取完成\n') 
         
-    def getAllUrl(self):
+    def getAllYearsUrl(self):
+        #柏林电影节第一届网页有问题，没有历届回顾，需要加偏移值
         URL = self.DoubanURL + str(1) + '/'
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'}
         res = requests.get(URL)
@@ -50,41 +40,40 @@ class MovieFestival:
         for ya in soup.find_all(id='year_awards'):
             for ul in ya.find_all('ul'):
                 for li in ul.find_all('li'):
-                    self.getSingleUrl(int(li.a.string))
-                    time.sleep(1.75)
-
-    def getUrlInRange(self, StartYear, EndYear):
-        for x in range(StartYear, EndYear + 1):
-            self.getSingleUrl(x)  
-            time.sleep(1.75)
+                    self.getSingleYearUrl(int(li.a.string), li.a.get('href'))
+                    time.sleep(1.5)
 
     def main(self):
-        if self.getAll:
-            self.getAllUrl()
-            return
-        else:
-            self.getUrlInRange(self.StartYear, self.EndYear)
-            return
+        self.getAllYearsUrl()
+        
+class DummyBrowser:
+    def __init__(self):
+        self.browser = webdriver.Chrome('/path/to/chromedriver')
+    def main(self):
+        self.browser.get('http://www.douban.com/')
+        driver = webdriver.Chrome('/path/to/chromedriver')  # Optional argument, if not specified will search path.
+        driver.get('http://www.google.com/xhtml');
+        time.sleep(5) # Let the user actually see something!
+        search_box = driver.find_element_by_name('q')
+        search_box.send_keys('ChromeDriver')
+        search_box.submit()
+        time.sleep(5) # Let the user actually see something!
+        driver.quit()
 
-Berlin = MovieFestival()
-Berlin.StartYear = 2015
-Berlin.main()
 
-Venice = MovieFestival()
-Venice.FestivalTitle = '威尼斯电影节'
-Venice.AwardTitle = ['金狮奖']
-Venice.DoubanURL = 'https://movie.douban.com/awards/venice'
-Venice.FirstYear = 1932
-Venice.LeakYear = [1933, 1943, 1944, 1945]#@TODO
-Venice.getAll = True
-Venice.main()
+#Berlin = MovieFestival()
+#Berlin.main()
 
-Cannes = MovieFestival()
-Cannes.FestivalTitle = '戛纳电影节'
-Cannes.AwardTitle = ['金棕榈奖']
-Cannes.DoubanURL = 'https://movie.douban.com/awards/cannes/'
-Cannes.FirstYear = 1946
-Cannes.LeakYear = [1948, 1950]
-Cannes.getAll = True
-Cannes.main()
+#Venice = MovieFestival()
+#Venice.FestivalTitle = '威尼斯电影节'
+#Venice.AwardTitle = ['金狮奖']
+#Venice.DoubanURL = 'https://movie.douban.com/awards/venice/'
+#Venice.main()
 
+#Cannes = MovieFestival()
+#Cannes.FestivalTitle = '戛纳电影节'
+#Cannes.AwardTitle = ['金棕榈奖']
+#Cannes.DoubanURL = 'https://movie.douban.com/awards/cannes/'
+#Cannes.main()
+
+dummyHand = DummyBrowser()
